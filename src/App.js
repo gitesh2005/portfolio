0619ede5226f05ba55fc132416af37e04348de61
Gitesh Sorout <giteshsorout@gmail.com>
2025-06-30 23:04:42 +0530

    Removed unused imports to fix Netlify build error

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { Mail, Linkedin } from "lucide-react";
 
 const skills = [
   "Excel", "Python", "Pandas", "Matplotlib", "Java",
